640c950bf9d4dc8e2178e3b7a1230facece510de
AM Rabby <42492989+rabby720@users.noreply.github.com>
2026-03-18 15:40:11 +0600

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-18T09:40:08.871Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-18T09:40:14.550Z
+Commit: Resolve null pointer exception in user service
